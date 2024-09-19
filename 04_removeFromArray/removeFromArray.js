8633a4da0933042a51415f73a684ba9b3837e2cd
Tamtai <107045052+Tainyz@users.noreply.github.com>
2024-09-19 10:49:05 +0700

    done 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayVal,...delVal) {
+    let arrLength = arrayVal.length;
+    let result = []
+    arrayVal.forEach(value => {
+        if(!delVal.includes(value)){
+            result.push(value);
+        }        
+    });
+
+
+    return result;
+
 };
 
 // Do not edit below this line
