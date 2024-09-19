3f7e670ab2692eac8e1bf8d0eca1f6e1674dabb7
Tamtai <107045052+Tainyz@users.noreply.github.com>
2024-09-19 10:19:28 +0700

    done 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(strVal,num) {
+    let resultStr = "";
+    if(num<0){
+        return "ERROR";        
+    }
+    for(let i=0;i<num;i++){
+        resultStr += strVal;
+    }
+    return resultStr;
 
 };
 
